836544909f19b0dd49d4bd93df060214225a1810
Nancyrecinos2026 <202205103@icvh.edu.gt>
2026-09-23 20:52:44 -0600

    Add files via upload

--- videojuego.py
+++ videojuego.py
@@ -28,5 +28,47 @@ else:
     print("Esperando turno...")
     print("Volviendo a JUGAR...")
 
+print("\nTURNO DEL ENEMIGO")
+print("1. Nimcy el Mapache")
+print("2. Cris el Topo")
+print("3. Cuac el Pato")
+enemigo = input("Elige un enemigo: ")
+if enemigo == "1":
+    print("Nimcy roba un bloque.")
+elif enemigo == "2":
+    print("Cris emerge de la tierra.")
+elif enemigo == "3":
+    print("Cuac destruye dos bloques.")
+else:
+    print("Enemigo no válido.")
 
+print("\nVERIFICAR DERROTA")
+
+princesa = input("¿La princesa recibió daño? (si/no): ")
+
+if princesa == "si":
+    print("¡La princesa recibió daño!")
+else:
+    print("La princesa está a salvo.")
+
+base = input("¿La base fue destruida? (si/no): ")
+
+if base == "si":
+    print("GAME OVER")
+    print("La base fue destruida.")
+else:
+    print("La base sigue en pie.")
+
+if princesa == "no" and base == "no":
+
+    print("\nVERIFICAR VICTORIA")
+
+    enemigo_derrotado = input("¿Derrotaste al enemigo? (si/no): ")
+
+    if enemigo_derrotado == "si":
+        print("¡GANASTE EL NIVEL 1!")
+        print("Recibes monedas.")
+    else:
+        print("No derrotaste al enemigo.")
+        print("Pierdes.")
 
